50d15eb474d25e55e1525a636ce9149404646093
Kike <bitbucket@lemambo.com>
2016-10-20 15:13:06 +0200

    buddybuild.com service build help file
    + Update to recommended instructions
    + More verbose

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+echo "BEGIN pwd:"
+pwd
+
+echo "git submodules ops"
+git submodule init
+git submodule update
+
 cd Frameworks
+echo "curl Blink-Frameworks"
 curl -OL https://github.com/blinksh/blink/releases/download/v1.019/Blink-Frameworks.tar.gz
-tar -zxf Blink-Frameworks.tar.gz
+echo "tar"
+tar -zxvf Blink-Frameworks.tar.gz
+
+echo "END"
