904b005ee546e5eede87796f598e6d2c836d5b76
kikeenrique <kikeenrique@users.noreply.github.com>
2017-06-12 11:25:00 +0200

    Update buddybuild compilation steps

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -3,14 +3,11 @@
 echo "BEGIN pwd:"
 pwd
 
-echo "git submodules ops"
-git submodule init
-git submodule update
+echo "git clone and submodules"
+git clone --recursive git@github.com:blinksh/blink.git
 
-cd Frameworks
-echo "curl Blink-Frameworks"
-curl -OL https://github.com/blinksh/blink/releases/download/v1.019/Blink-Frameworks.tar.gz
-echo "tar"
-tar -zxvf Blink-Frameworks.tar.gz
+cd blink 
+echo "get frameworks"
+./get_frameworks.sh
 
 echo "END"
