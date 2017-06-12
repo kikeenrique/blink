922f0e1cdc4d48f73b883add3c4d5889bea65014
kikeenrique <kikeenrique@users.noreply.github.com>
2017-06-12 11:39:33 +0200

    Fix buddybuild compilation
    + No need to clone, as buddybuild clones by default. Just update submodules.

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -2,11 +2,15 @@
 
 echo "BEGIN pwd:"
 pwd
+echo "list prebuild initial directory"
+ls -la 
 
-echo "git clone and submodules"
-git clone --recursive git@github.com:blinksh/blink.git
 
+echo "git get submodules"
 cd blink 
+#git clone --recursive git@github.com:blinksh/blink.git
+git submodule update --init --recursive
+
 echo "get frameworks"
 ./get_frameworks.sh
 
